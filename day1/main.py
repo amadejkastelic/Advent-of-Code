4535cbbd3e2906374a12441d56db57f14619bed5
Amadej Kastelic <amadej.kastelic@bitstamp.net>
2021-12-01 11:39:31 +0100

    day1 optimization

--- day1/main.py
+++ day1/main.py
@@ -1,22 +1,21 @@
-def count_increases(depths, sliding_window=1):
-    sliding_depths = []
-    for i in range(len(depths)):
-        sliding_depths.append(sum(depths[i:i+sliding_window]))
-
-    count = 0
-    for i in range(1, len(sliding_depths)):
-        if sliding_depths[i] > sliding_depths[i-1]:
-            count += 1
-    return count
+import typing
 
 
-if __name__ == '__main__':
+def main():
     file_name = input("Enter input path: ")
 
     with open(file_name, 'r') as f:
         content = f.read().splitlines()
 
-    depths = [int(depth) for depth in content]
+    depths = list(map(int, content))
 
-    print(count_increases(depths=depths))
-    print(count_increases(depths=depths, sliding_window=3))
+    print(_count_increases(depths=depths))
+    print(_count_increases(depths=depths, sliding_window=3))
+
+
+def _count_increases(depths: typing.List[int], sliding_window: int = 1) -> int:
+    return sum([1 for i, x in enumerate(depths[sliding_window:]) if x > depths[i]])
+
+
+if __name__ == '__main__':
+    main()
