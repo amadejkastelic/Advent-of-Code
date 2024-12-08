3f19914f076167870d1b69af4f255e73480bfdb9
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-12-08 21:03:20 +0100

    Add init command

---
+++ examples/template.py
@@ -0,0 +1,14 @@
+import solver
+
+
+class Solver(solver.Solver):
+    def _init(self):
+        with open(self.input_file_path) as f:
+            for line in f.readlines():
+                pass
+
+    def _solve_part1(self) -> int:
+        pass
+
+    def _solve_part2(self) -> int:
+        pass
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import dataclasses
 import datetime
 import importlib
+import os
+import shutil
 import sys
 import typing
 
@@ -14,6 +16,7 @@ class Config:
     day: int
     part: typing.Optional[int] = None
     input_file: typing.Optional[str] = None
+    init: bool = False
 
     @classmethod
     def from_arguments(cls) -> 'Config':
@@ -21,28 +24,34 @@ class Config:
 
         parser = argparse.ArgumentParser()
         parser.add_argument(
-            "--year",
+            '--year',
             type=int,
-            help="Year of the puzzle",
+            help='Year of the puzzle',
             default=today.year,
         )
         parser.add_argument(
-            "--day",
+            '--day',
             type=int,
-            help="Day of the puzzle",
+            help='Day of the puzzle',
             default=today.day,
         )
         parser.add_argument(
-            "--part",
+            '--part',
             type=typing.Optional[int],
-            help="Part of the puzzle",
+            help='Part of the puzzle',
             default=None,
         )
         parser.add_argument(
-            "--input",
+            '--input',
             type=str,
-            help="Input file name",
+            help='Input file name',
             default='input.txt',
+        )
+        parser.add_argument(
+            '--init',
+            type=bool,
+            help='Prepare stuff',
+            default=False,
         )
         args = parser.parse_args()
 
@@ -50,19 +59,26 @@ class Config:
             year=args.year,
             day=args.day,
             part=args.part,
-            input_file=f"inputs/{args.year}/day{args.day}/{args.input}",
+            input_file=f'inputs/{args.year}/day{args.day}/{args.input}',
+            init=args.init,
         )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     config = Config.from_arguments()
+
+    if config.init:
+        path = os.path.join(config.year, f'day{config.day}')
+        path = f'{config.year}/day{config.day}'
+        os.makedirs(path, exist_ok=True)
+        shutil.copy2(src='examples/template.py', dst=os.path.join(path, 'solver.py'))
 
     try:
         s: solver.Solver = importlib.import_module(
-            f"{config.year}.day{config.day}.solver",
+            f'{config.year}.day{config.day}.solver',
         ).Solver(config.input_file)
     except ImportError:
-        print("Invalid year or day")
+        print('Invalid year or day')
         sys.exit(1)
 
     s.solve(config.part)
