db906ae4abe95571e6fb0952bc4a4ce146e8fd5e
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-12-08 09:30:25 +0000

    day8

--- 2024/day8/solver.py
+++ 2024/day8/solver.py
@@ -18,10 +18,34 @@ class Solver(solver.Solver):
                         self.antennas[ch].append((j, i))
         self.y = len(self.map)
         self.x = len(self.map[0])
+        self.surrounding_cache = {}
 
     def _in_bounds(self, point: typing.Tuple[int, int]) -> bool:
         x, y = point
         return x >= 0 and x < self.x and y >= 0 and y < self.y
+
+    def _get_line(self, y: int) -> typing.List[str]:
+        if y < 0 or y >= self.y:
+            return ['.']
+        return self.map[y]
+
+    def _get_col(self, x: int) -> typing.List[str]:
+        if x < 0 or x >= self.x:
+            return ['.']
+        return [self.map[i][x] for i in range(self.y)]
+
+    def _has_antenna_in_line(self, point: typing.Tuple[int, int]) -> bool:
+        if point not in self.surrounding_cache:
+            self.surrounding_cache[point] = not all(
+                [
+                    ch == '.'
+                    for ch in self._get_col(point[0] - 1)
+                    + self._get_col(point[0] + 1)
+                    + self._get_line(point[1] - 1)
+                    + self._get_line(point[1] + 1)
+                ]
+            )
+        return self.surrounding_cache[point]
 
     def _solve_part1(self) -> int:
         antinodes = set()
@@ -45,28 +69,36 @@ class Solver(solver.Solver):
 
     def _solve_part2(self) -> int:
         antinodes = set()
+
         for positions in self.antennas.values():
             for a, b in combinations(positions, 2):
                 distance = (abs(a[0] - b[0]), abs(a[1] - b[1]))
+
+                if self._has_antenna_in_line(a):
+                    antinodes.add(a)
+
+                if self._has_antenna_in_line(b):
+                    antinodes.add(b)
+
                 if a[0] > b[0]:
-                    tmp_dist = distance[:]
-                    while a[0] + tmp_dist[0] < self.x and a[1] - tmp_dist[1] >= 0:
-                        antinodes.add((a[0] + tmp_dist[0], a[1] - tmp_dist[1]))
-                        tmp_dist = (tmp_dist[0] + distance[0], tmp_dist[1] + distance[1])
-                    tmp_dist = distance[:]
-                    while b[0] - tmp_dist[0] >= 0 and b[1] + tmp_dist[1] < self.y:
-                        antinodes.add((b[0] - tmp_dist[0], b[1] + tmp_dist[1]))
-                        tmp_dist = (tmp_dist[0] + distance[0], tmp_dist[1] + distance[1])
-                elif a[0] == b[0] or a[1] == b[1]:
-                    pass
+                    antinode = (a[0] + distance[0], a[1] - distance[1])
+                    while self._in_bounds(antinode):
+                        antinodes.add(antinode)
+                        antinode = (antinode[0] + distance[0], antinode[1] - distance[1])
+
+                    antinode = (b[0] - distance[0], b[1] + distance[1])
+                    while self._in_bounds(antinode):
+                        antinodes.add(antinode)
+                        antinode = (antinode[0] - distance[0], antinode[1] + distance[1])
                 else:
-                    tmp_dist = distance[:]
-                    while a[0] - tmp_dist[0] >= 0 and a[1] - tmp_dist[1] >= 0:
-                        antinodes.add((a[0] - tmp_dist[0], a[1] - tmp_dist[1]))
-                        tmp_dist = (tmp_dist[0] + distance[0], tmp_dist[1] + distance[1])
-                    tmp_dist = distance[:]
-                    while b[0] + tmp_dist[0] < self.x and b[1] + tmp_dist[1] < self.y:
-                        antinodes.add((b[0] + tmp_dist[0], b[1] + tmp_dist[1]))
-                        tmp_dist = (tmp_dist[0] + distance[0], tmp_dist[1] + distance[1])
-        print(antinodes)
+                    antinode = (a[0] - distance[0], a[1] - distance[1])
+                    while self._in_bounds(antinode):
+                        antinodes.add(antinode)
+                        antinode = (antinode[0] - distance[0], antinode[1] - distance[1])
+
+                    antinode = (b[0] + distance[0], b[1] + distance[1])
+                    while self._in_bounds(antinode):
+                        antinodes.add(antinode)
+                        antinode = (antinode[0] + distance[0], antinode[1] + distance[1])
+
         return len(antinodes)
