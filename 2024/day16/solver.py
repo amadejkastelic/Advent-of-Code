8a6859f538c4b0233abed62f0150367231897684
Amadej Kastelic <amadejkastelic7@gmail.com>
2024-12-16 09:36:11 +0000

    Day 16

--- 2024/day16/solver.py
+++ 2024/day16/solver.py
@@ -1,3 +1,4 @@
+import functools
 import sys
 import typing
 from collections import defaultdict
@@ -24,11 +25,13 @@ class Solver(solver.Solver):
                 elif ch == 'E':
                     self.end = (i, j)
 
+    @functools.lru_cache(maxsize=100000)
     def get(self, x: int, y: int) -> str:
         if x < 0 or y < 0 or y >= len(self.grid) or x >= len(self.grid[0]):
             return '#'
         return self.grid[y][x]
 
+    @functools.lru_cache(maxsize=100000)
     def get_neighbors(
         self,
         x: int,
@@ -39,7 +42,9 @@ class Solver(solver.Solver):
 
     def _solve_part1(self) -> int:
         distances = defaultdict(lambda: sys.maxsize)
+        distances_dir = defaultdict(lambda: sys.maxsize)
         distances[self.start] = 0
+        distances_dir[(self.start, (1, 0))] = 0
         q = [(self.start, 0, (1, 0))]
         while q:
             cur, cost, dir = q.pop(0)
@@ -55,36 +60,34 @@ class Solver(solver.Solver):
                 distance = cost + nc
                 if distance < distances[(nx, ny)] and self.get(nx, ny) != '#':
                     distances[(nx, ny)] = distance
+                    distances_dir[(nx, ny), dir] = distance
                     q.append(((nx, ny), distance, nd))
 
-        return distances[self.end]
+        self.end_dist = distances[self.end]
+        self.distances = distances_dir
+        return self.end_dist
 
     def _solve_part2(self) -> int:
-        end_dists = defaultdict(set)
-        paths = defaultdict(set)
+        visited = set()
         q = [(self.start, 0, (1, 0), set())]
         while q:
             cur, cost, dir, path = q.pop(0)
 
-            path = path.union({cur})
-
-            frozen = frozenset(path)
-            if frozen in paths[cur]:
+            if cost > self.distances[(cur, dir)]:
                 continue
 
-            paths[cur].add(frozen)
+            if cost > self.end_dist:
+                continue
+
+            next_path = path.union({cur})
 
             if cur == self.end:
-                end_dists[cost].update(path)
-                print(len(end_dists))
+                visited.update(next_path)
                 continue
 
-            for nx, ny, nc, nd in self.get_neighbors(
-                cur[0],
-                cur[1],
-                dir,
-            ):
-                if self.get(nx, ny) != '#' and (nx, ny) not in path:
-                    q.append(((nx, ny), cost + nc, nd, path))
+            for nx, ny, nc, nd in self.get_neighbors(cur[0], cur[1], dir):
+                distance = cost + nc
+                if distance <= self.end_dist and (nx, ny) not in path and self.get(nx, ny) != '#':
+                    q.append(((nx, ny), distance, nd, next_path))
 
-        return len(end_dists[min(end_dists.keys())])
+        return len(visited)
